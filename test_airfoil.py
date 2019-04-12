2f3663d2426d705f17bd94381ec482bd576de6e2
Neil Wu <neilwu@umich.edu>
2019-04-12 17:58:30 -0400

    added alpha to Ub and cl/cd computation

--- test_airfoil.py
+++ test_airfoil.py
@@ -1,4 +1,4 @@
-""" main script for proj2 """
+""" main script for proj4 """
 import numpy as np
 import matplotlib.pyplot as plt
 from meshes import Mesh
@@ -12,6 +12,7 @@ airfoil = Mesh('meshes/naca0012.gri')
 # airfoil.plot()
 # plt.show()
 
-CFDSolver = DGSolver(airfoil, order=0)
+CFDSolver = DGSolver(airfoil, order=0,alpha=2)
 CFDSolver.solve()
 CFDSolver.writeSolution('airfoil')
+CFDSolver.postprocess()
